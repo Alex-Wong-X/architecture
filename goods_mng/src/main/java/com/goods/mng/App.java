d5629874128378caec9aff95fcee3a0ea7257f3f
Alex-Wong-X <wangsh36@126.com>
2018-05-21 00:47:36 +0800

    用我所学的几乎全部web知识构建的一个实验性质的网站，我希望持续更新和完善她

---
+++ goods_mng/src/main/java/com/goods/mng/App.java
@@ -0,0 +1,13 @@
+package com.goods.mng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
